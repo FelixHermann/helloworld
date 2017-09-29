ac0639e46cb97b64c139a38ce442e0043855644b
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2017-09-29 08:38:34 +0200

    add starter

---
+++ src/helloworld/Console.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Console implements Publisher {
+
+	@Override
+	public void publishText(String toPublish) {
+		System.out.println(toPublish);
+	}
+}
---
+++ src/helloworld/Starter.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		new App(getConfiguration()).publishHelloWorld();
+	}
+	
+	private static Configuration getConfiguration() {
+		ConfigurationImpl config = new ConfigurationImpl();
+		config.setPublisher(new Console());
+		return config;
+	}
+}
